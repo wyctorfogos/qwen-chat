568ecd58b197aa3999fefb730e2472a86f554cf1
wyctorfogos <wyctor.fogos@gmail.com>
2025-08-16 09:56:52 -0300

    feat: Interação com o usuário via terminal

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ if __name__=="__main__":
 	device = "cpu" ## "cuda" if torch.cuda.is_available() else "cpu"
 	new_model = LLM(llm_model_name="Qwen/Qwen2.5-1.5B-Instruct", device=device, max_new_tokens=512)
     
-	# Conteúdo digitado pelo usuário
-	user_message = "Qual seu nome?"
-	print(f"Resposta do modelo: {new_model.llm_response(user_message)}")
+	while True:
+		# Conteúdo digitado pelo usuário
+		user_message = input(f"Digite o que deseja saber:\n") # "Qual seu nome?"
+		if user_message=="bye":
+			break
+		print(f"Resposta do modelo: {new_model.llm_response(user_message)}. Digite 'bye' para finalizar a conversa.")
