4bd5d0928a3dc47ce7fe0bcb85254984de0290ee
wyctorfogos <wyctor.fogos@gmail.com>
2025-08-17 23:32:53 -0300

    feat: Add history of the user data

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ import torch
 from models.LLM import LLM
 
 if __name__=="__main__":
-	device = "cpu" ## "cuda" if torch.cuda.is_available() else "cpu"
-	new_model = LLM(llm_model_name="Qwen/Qwen2.5-1.5B-Instruct", device=device, max_new_tokens=512)
-    
+	device = "cuda" if torch.cuda.is_available() else "cpu"
+	new_model = LLM(llm_model_name="Qwen/Qwen2.5-0.5B-Instruct", device=device, max_new_tokens=512)
+	print("Digite 'bye' para finalizar a conversa.")
+	user_message_history = []
 	while True:
 		# Conteúdo digitado pelo usuário
 		user_message = input(f"Digite o que deseja saber:\n") # "Qual seu nome?"
 		if user_message=="bye":
 			break
-		print(f"Resposta do modelo: {new_model.llm_response(user_message)}. Digite 'bye' para finalizar a conversa.")
+		user_message_history.append(user_message+f"Resposta do modelo: \n{new_model.llm_response(user_message)}")
+		print(f"Resposta do modelo: \n{user_message_history[-1]}")
